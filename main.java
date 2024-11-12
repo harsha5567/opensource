85098c7c4d9410b85f7f940b540e504658e43d31
harsha5567 <yerraharshavardhan0@gmail.com>
2024-11-12 10:29:30 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+#import java.util.scanner;
+class main{
+  public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    int x=sc.nextInt();
+    if(x%2){
+      System.out.println("EVEN");
+    }else{
+      System.out.println("ODD");
+    }
+  }
+}
